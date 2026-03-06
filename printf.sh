dedb97019478a55d6e99f5457759d12439aa5971
nico <nio@denios.de>
2026-03-06 11:43:13 +0100

    add-error-handling

--- printf.sh
+++ printf.sh
@@ -4,10 +4,16 @@ green="\e[32m"
 yellow="\e[33m"
 red="\e[31m"
 
+if [ $# -eq 0 ]; then
+	printf 'Es wurde kein Parameter übergeben!\n'
+	printf 'Verwendung: %s <dein Parameter>\n' "$0"
+	exit 1
+fi
+
 programmname"$0"
 param="$1"
 
-printf "The current Time: $(date '+%H:%M:%S')\n"
+printf 'The current Time: $(date '+%H:%M:%S')\n'
 
 printf "Das hier ist Programm: %s\n" '$programmname'
 printf "Als Parameter wurde %s übergeben\n" '$param'
