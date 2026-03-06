cad626d91558cd964d050efb92bc5a04ed1515de
nico <nio@denios.de>
2026-03-06 11:21:10 +0100

    add context to printf.sh

---
+++ printf.sh
@@ -0,0 +1,10 @@
+green="\e[32m"
+yellow="\e[33m"
+red="\e[31m"
+
+
+printf "The current Time: $(date '+%H:%M:%S')\n"
+
+printf "${yellow}Do you want to start the deletion of Home-Directory?${reset} [y/N]\n"
+
+printf "${red}Whatever"
