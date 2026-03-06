7eef9c8813ef5a21476646196f37f23a56573cd3
nico <nio@denios.de>
2026-03-06 11:32:27 +0100

    updated-printf.sh

--- printf.sh
+++ printf.sh
@@ -1,10 +1,17 @@
+#!/usr/bin/sh
+
 green="\e[32m"
 yellow="\e[33m"
 red="\e[31m"
 
+programmname"$0"
+param="$1"
 
 printf "The current Time: $(date '+%H:%M:%S')\n"
 
+printf "Das hier ist Programm: %s\n" '$programmname'
+printf "Als Parameter wurde %s übergeben\n" '$param'
+
 printf "${yellow}Do you want to start the deletion of Home-Directory?${reset} [y/N]\n"
 
-printf "${red}Whatever"
+
